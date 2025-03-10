d4d5369eb01bd92bc17b013a4a373e2ed7a60300
MidnightSuspect <103286396+MidnightGambit@users.noreply.github.com>
2025-03-10 15:17:10 -0700

    Update arithmeticformatter.py

--- arithmeticformatter.py
+++ arithmeticformatter.py
@@ -1,52 +1,54 @@
-def arithmetic_arranger(problems, val=False):
-    arranged_problems = ''
+def arithmetic_arranger(problems, show_answers=False):
+    # Check if there are too many problems
     if len(problems) > 5:
-        arranged_problems = "Error: Too many problems."
-        return arranged_problems
-
-    # list of all operations in str format
-    operations = list(map(lambda x: x.split()[1], problems))
-    if set(operations) != {'+', '-'} and len(set(operations)) != 2:
-        arranged_problems = "Error: Operator must be '+' or '-'."
-        return arranged_problems
-
-    numbers = []  # list of all operands in str format
-    for i in problems:
-        p = i.split()
-        numbers.extend([p[0], p[2]])
-
-    if not all(map(lambda x: x.isdigit(), numbers)):
-        arranged_problems = "Error: Numbers must only contain digits."
-        return arranged_problems
-
-    if not all(map(lambda x: len(x) < 5, numbers)):
-        arranged_problems = "Error: Numbers cannot be more than four digits."
-        return arranged_problems
-
-    top_row = ''
-    dashes = ''
-    values = list(map(lambda x: eval(x), problems))
-    solutions = ''
-    for i in range(0, len(numbers), 2):
-        space_width = max(len(numbers[i]), len(numbers[i+1])) + 2
-        top_row += numbers[i].rjust(space_width)
-        dashes += '-' * space_width
-        solutions += str(values[i // 2]).rjust(space_width)
-        if i != len(numbers) - 2:
-            top_row += ' ' * 4
-            dashes += ' ' * 4
-            solutions += ' ' * 4
-
-    bottom_row = ''
-    for i in range(1, len(numbers), 2):
-        space_width = max(len(numbers[i - 1]), len(numbers[i])) + 1
-        bottom_row += operations[i // 2]
-        bottom_row += numbers[i].rjust(space_width)
-        if i != len(numbers) - 1:
-            bottom_row += ' ' * 4
-
-    if val:
-        arranged_problems = '\n'.join((top_row, bottom_row, dashes, solutions))
+        return "Error: Too many problems."
+    
+    # Initialize the strings for the lines of the result
+    top_row = ""
+    second_row = ""
+    dashes_row = ""
+    answers_row = ""
+    
+    for problem in problems:
+        # Split each problem into operands and operator
+        parts = problem.split()
+        
+        # Validate if the operator is correct
+        if parts[1] not in ['+', '-']:
+            return "Error: Operator must be '+' or '-'."
+        
+        # Validate if the operands are digits
+        if not parts[0].isdigit() or not parts[2].isdigit():
+            return "Error: Numbers must only contain digits."
+        
+        # Validate if the operands are within the length limit
+        if len(parts[0]) > 4 or len(parts[2]) > 4:
+            return "Error: Numbers cannot be more than four digits."
+        
+        # Format the top row, second row and dashes
+        width = max(len(parts[0]), len(parts[2])) + 2  # Operator and space
+        
+        top_row += f"{parts[0]:>{width}}    "
+        second_row += f"{parts[1]} {parts[2]:>{width - 2}}    "
+        dashes_row += '-' * width + "    "
+        
+        # If show_answers is True, calculate and format the answers
+        if show_answers:
+            if parts[1] == '+':
+                result = str(int(parts[0]) + int(parts[2]))
+            else:
+                result = str(int(parts[0]) - int(parts[2]))
+            answers_row += f"{result:>{width}}    "
+    
+    # Remove the extra space at the end of each row
+    top_row = top_row.rstrip()
+    second_row = second_row.rstrip()
+    dashes_row = dashes_row.rstrip()
+    if show_answers:
+        answers_row = answers_row.rstrip()
+    
+    # Return the final formatted string
+    if show_answers:
+        return f"{top_row}\n{second_row}\n{dashes_row}\n{answers_row}"
     else:
-        arranged_problems = '\n'.join((top_row, bottom_row, dashes))
-    return arranged_problems
+        return f"{top_row}\n{second_row}\n{dashes_row}"
